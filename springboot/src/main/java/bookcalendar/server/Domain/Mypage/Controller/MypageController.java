89938de569907fb8f07a460f12a273c0f007507a
passionryu <rsy1225@naver.com>
2025-05-08 00:28:40 +0900

    ✨ [기능 구현] : 마이페이지에서 현재 내 독서 권수, 독후감 작성 수 조회 API 구현 완료
    다만, 현재 내 독서 권수가 자동 증가되고 있지 않는 문제가 있음, 추후 수정할 것

--- springboot/src/main/java/bookcalendar/server/Domain/Mypage/Controller/MypageController.java
+++ springboot/src/main/java/bookcalendar/server/Domain/Mypage/Controller/MypageController.java
@@ -308,25 +308,28 @@ public class MypageController {
                 .body(new ApiResponseWrapper<>("장바구니 취소된 도서의 장바구니 고유 번호 : " + cartId,"장바구니의 책이 정상적으로 취소 되었습니다."));
     }
 
-    // ======================= Search Engine(검색 엔진) =========================
+    // ======================= ETC =========================
 
     /**
-     * 독후감 검색 API
+     * 독서 수 & 독후감 작성 수 조회 API
      *
-     * @param keyword 검색 키워드
-     * @return 검색된 독후감 리스트
+     * @param customUserDetails 인증된 유저의 정보 객체
+     * @return 독서 수 & 독후감 작성 수 DTO 반환
      */
-//    @Operation(summary = "독후감 검색 API",description = "독후감 검색 기능이다.",
-//            responses  ={
-//                    @ApiResponse(responseCode = "200", description = "입력하신 키워드로 독후감이 성공적으로 검색되었습니다."),
-//                    @ApiResponse(responseCode = "401",description = "엑세스 토큰 만료"),
-//                    @ApiResponse(responseCode = "500", description = "서버 내부 오류")
-//            })
-//    @PostMapping("/search/review")
-//    public ResponseEntity<ApiResponseWrapper<List<MyReviewList>>> searchReview(@RequestParam String keyword){
-//
-//
-//        return ResponseEntity.status(HttpStatus.OK)
-//                .body(new ApiResponseWrapper<>(null, "입력하신 키워드로 독후감이 성공적으로 검색되었습니다."));
-//    }
+    @Operation(summary = "독서 수 & 독후감 작성 수 조회 API", description = "독서 수와 독후감 작성 수를 조회하는 기능이다.  ",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "독서 수와 독후감 작성수가 정상적으로 되었습니다."),
+                    @ApiResponse(responseCode = "401", description = "유저 인증 오류"),
+                    @ApiResponse(responseCode = "500", description = "서버 내부 오류")
+            })
+    @GetMapping("/statistics")
+    public ResponseEntity<ApiResponseWrapper<StatisticResponse>> getStatistics(@AuthenticationPrincipal CustomUserDetails customUserDetails){
+
+        // 독서 수 & 독후감 작성 수 조회 서비스 레이어 호출
+        StatisticResponse statisticResponse = mypageService.getStatistic(customUserDetails);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new ApiResponseWrapper<>(statisticResponse,"독서 수와 독후감 작성수가 정상적으로 되었습니다."));
+    }
+
 }
---
+++ springboot/src/main/java/bookcalendar/server/Domain/Mypage/DTO/Response/StatisticResponse.java
@@ -0,0 +1,7 @@
+package bookcalendar.server.Domain.Mypage.DTO.Response;
+
+public record StatisticResponse(
+        Integer bookCount,
+        Integer reviewCount
+) {
+}

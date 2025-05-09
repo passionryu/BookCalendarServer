99f5ce8ef1e7c4b4cbda283f7d4276f700ac213a
passionryu <rsy1225@naver.com>
2025-05-10 01:22:21 +0900

    ♻️ [리첵토링] : Member 도메인 서비스 레이어 리펙토링 + 회원가입 시 현재 날짜 기록 서비스 추가
    리펙토링 진행

---
+++ springboot/src/main/java/bookcalendar/server/Domain/Member/Helper/MemberHelper.java
@@ -0,0 +1,9 @@
+package bookcalendar.server.Domain.Member.Helper;
+
+import bookcalendar.server.Domain.Member.Entity.Member;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MemberHelper {
+
+}

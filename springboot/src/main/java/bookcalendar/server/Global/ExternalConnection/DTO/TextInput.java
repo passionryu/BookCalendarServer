3913638a681351a651bfd1cd51cbbfa443a34061
passionryu <rsy1225@naver.com>
2025-05-16 01:01:28 +0900

    🐛 [버그 수정] : DTO에 기본 생성자 추가
    4차 수정

--- springboot/src/main/java/bookcalendar/server/Global/ExternalConnection/DTO/TextInput.java
+++ springboot/src/main/java/bookcalendar/server/Global/ExternalConnection/DTO/TextInput.java
@@ -2,8 +2,10 @@ package bookcalendar.server.global.ExternalConnection.DTO;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data // @Getter, @Setter, @NoArgsConstructor 포함
+@NoArgsConstructor
 @AllArgsConstructor
 public class TextInput {
     private String text;
